dd495e43deb9f88c24469f5861109ae329ee2e07
IgnacioCava <cavaignacio11@gmail.com>
2023-08-18 08:08:31 -0300

    feat: complete createconversation mutation

--- src/graphql/typeDefs/index.ts
+++ src/graphql/typeDefs/index.ts
@@ -1,6 +1,7 @@
-import userTypeDefs from './user.js'
 import conversationTypeDefs from './conversation.js'
+import messageTypeDefs from './messages.js'
+import userTypeDefs from './user.js'
 
-const typeDefs = [userTypeDefs, conversationTypeDefs]
+const typeDefs = [userTypeDefs, conversationTypeDefs, messageTypeDefs]
 
 export default typeDefs
